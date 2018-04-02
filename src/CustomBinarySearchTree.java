7c68446b9a31592aa49d88cea3f6b4777986cd8e
rajesh-android-dev <urajesh.android@gmail.com>
2018-04-02 17:32:15 +0530

    Custom class for Binary Search tree added

---
+++ src/CustomBinarySearchTree.java
@@ -0,0 +1,125 @@
+/**
+ * Custom class of a Binary search tree.
+ */
+
+public class CustomBinarySearchTree {
+
+    private Node root;
+
+    public CustomBinarySearchTree() {
+        root = null;
+    }
+
+    /**
+     * Inserts a new item in BST
+     *
+     * @param item : item to be inserted
+     * @return
+     */
+    public boolean insert(Person item) {
+        Node n = new Node();
+        n.data = item;
+        n.lc = null;
+        n.rc = null;
+
+        if (root == null) {
+            root = n;
+            return true;
+        }
+
+        Node p = root;
+        Node c = root;
+        while (c != null) {
+            p = c;
+            if (item.getName().compareTo(c.data.getName()) < 0) {
+                c = c.lc;
+            } else {
+                c = c.rc;
+            }
+        }
+        if (item.getName().compareTo(p.data.getName()) < 0) {
+            p.lc = n;
+        } else {
+            p.rc = n;
+        }
+        return true;
+    }
+
+    /**
+     * returns the node for the given key
+     *
+     * @param key : key of the node
+     * @return
+     */
+    public Node findNode(String key) {
+        Node c = root;
+        while (c != null) {
+            if (key.compareTo(c.data.getName()) == 0) {
+                break;
+            }
+            if (key.compareTo(c.data.getName()) < 0) {
+                c = c.lc;
+            } else {
+                c = c.rc;
+            }
+        }
+
+        return c;
+    }
+
+    /**
+     * returns parent node of the given key
+     *
+     * @param key
+     * @return parent node
+     */
+    public Node findParent(String key) {
+        Node p = root;
+        Node c = root;
+        do {
+            if (key.compareTo(c.data.getName()) == 0) {
+                break;
+            }
+            p = c;
+            if (key.compareTo(c.data.getName()) < 0) {
+                c = c.lc;
+            } else {
+                c = c.rc;
+            }
+        } while (c != null);
+//        System.out.println(">>" + p.data.getName());
+        if (c != null) {
+            return p;
+        } else {
+            return null;
+        }
+    }
+
+    public Person getData(Node n) {
+        return n.data;
+    }
+
+    /**
+     * Prints child values of the given node in the tree
+     *
+     * @param n : parent node
+     */
+    public void showAll(Node n) {
+        Node p = n;
+        if (p != null) {
+            System.out.println(" " + p.data);
+            showAll(p.lc);
+            showAll(p.rc);
+        }
+    }
+
+    /**
+     * Node class
+     */
+    public class Node {
+        private Node lc;
+        private Person data;
+        private Node rc;
+
+    }
+}
--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,6 +1,24 @@
 public class MainProgram {
 
     public static void main(String[] args){
-        System.out.println(PrimeDecomp.factors(7775460));
+//        System.out.println(PrimeDecomp.factors(7775460));
+
+        //Binary Search tree
+        Person p1 = new Person("Rajesh", 25);
+        Person p2 = new Person("Vinu", 22);
+        Person p3 = new Person("Dinu", 23);
+        Person p4 = new Person("Sekar", 19);
+        Person p5 = new Person("Kumar", 17);
+
+        CustomBinarySearchTree bst = new CustomBinarySearchTree();
+        bst.insert(p1);
+        bst.insert(p2);
+        bst.insert(p3);
+        bst.insert(p4);
+        bst.insert(p5);
+
+        bst.showAll(bst.findNode("Rajesh"));
+        Person p = bst.getData(bst.findParent("Kumar"));
+        System.out.println("parent is " + p.toString());
     }
 }
