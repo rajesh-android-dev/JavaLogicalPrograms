9b40adad61ace843b06f6c4349c37b8911f908d2
Mac Mini <macmini@Macs-Mac-mini.local>
2018-05-14 20:09:48 +0530

    added three new classes

--- src/ZoDistinctNumber.java
+++ src/ZoDistinctNumber.java
@@ -1,2 +1,34 @@
-public class DistinctNumber {
+import java.util.Scanner;
+
+/**
+ * Find a distinct number N in an array, where the total count of numbers greater than N is equal to the N ex. { 1,1,4,7,9,10,11} = 4
+ */
+public class ZoDistinctNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        checkDistinctNum(arr);
+    }
+
+    private static void checkDistinctNum(int[] arr) {
+        int val = 0;
+        outerloop:
+        for (int i : arr) {
+            int rep = 0;
+            for (int j : arr) {
+                if (rep < 2) {
+                    if (j > i) val++;
+                    if (j == i) rep++;
+                } else {
+                    break;
+                }
+            }
+            if (val == i) break outerloop;
+        }
+        System.out.println(val);
+    }
 }
--- src/ZoSemiPrime.java
+++ src/ZoSemiPrime.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
+/**
+ * Find out if the give number is a semi prime or not
+ * Semi prime means the multiples of two prime number will produce the given number
+ */
 public class ZoSemiPrime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(isSemiPrime(sc.nextInt()));
+    }
+
+    public static boolean isSemiPrime(int n){
+        int cnt = 0;
+        for (int i = 2; cnt < 2 && i*i<=n;i++) {
+            while (n % i == 0) {
+                n /= i;
+                cnt++;
+            }
+        }
+            if (n > 1){
+                cnt++;
+            }
+        return cnt == 2;
+    }
 }
