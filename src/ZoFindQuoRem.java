c47f8c95770438c9928673e57d5083b3bea387d4
Mac Mini <macmini@Macs-Mac-mini.local>
2018-05-16 10:42:35 +0530

    new programs added

---
+++ src/ZoFindQuoRem.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Find quotient and reminder for a given number and divisor without using / or %
+ * i/p: number : 10, divisor : 2
+ * o/p: quotient : 5, reminder : 0
+ */
+public class ZoFindQuoRem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int div = sc.nextInt();
+        printQuoRem(num, div);
+    }
+
+    private static void printQuoRem(int num, int div) {
+        int quo = 1;
+        int rem;
+        int n = div;
+        while (true) {
+            quo++;
+            n += div;
+            if (n >= num) {
+                rem = n - num;
+                break;
+            }
+        }
+        System.out.println("Quo : " + quo + " rem : " + rem);
+    }
+}
